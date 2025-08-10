90ffe1edbb658e08885abeb064006c3b78a58f59
daehyun <kimdeahyeon5445@gmail.com>
2025-08-10 20:53:21 +0900

    [feat] add filtering to "get all orders" API and integrate QueryDSL with related repository changes

--- src/main/java/com/example/hanaro/repository/OrderRepository.java
+++ src/main/java/com/example/hanaro/repository/OrderRepository.java
@@ -4,10 +4,11 @@ import com.example.hanaro.entity.Order;
 import com.example.hanaro.entity.User;
 import com.example.hanaro.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.hanaro.repository.custom.OrderRepositoryCustom;
 
 import java.util.List;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
     List<Order> findAllByStatus(OrderStatus status);
     List<Order> findAllByUserOrderByCreatedAtDesc(User user);
     List<Order> findAllByOrderByCreatedAtDesc();
